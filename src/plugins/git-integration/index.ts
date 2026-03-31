1943da1ce13b8cf87cc1c8b3a55eafe66748132d
Benjamin Sirianni <ben@math-rad.com>
2026-03-31 00:38:45 -0500

    use lighting-fs and isomorphic-git

--- src/plugins/git-integration/index.ts
+++ src/plugins/git-integration/index.ts
@@ -1,5 +1,7 @@
 import { PluginController } from "#plugins/PluginController.ts";
 import { configList } from "./config";
+import git from "isomorphic-git";
+import LightningFS from "@isomorphic-git/lightning-fs";
 
 export default class GitIntegration extends PluginController {
   static id = "git-integration" as const;
